153ad4af93cca68adf51156088994cafb61df5f7
kkang_h00n <kig2454@gmail.com>
2025-04-17 12:03:14 +0900

    init commit

---
+++ src/main/java/swyp/team5/greening/GreeningApplication.java
@@ -0,0 +1,13 @@
+package swyp.team5.greening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreeningApplication.class, args);
+    }
+
+}
