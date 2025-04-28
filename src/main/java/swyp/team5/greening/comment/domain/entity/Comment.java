d0fba2365e6d2d83e9ec43812206de3212a33dd7
kkang_h00n <kig2454@gmail.com>
2025-04-28 20:58:08 +0900

    feat: 댓글 도메인 엔티티 셋팅

---
+++ src/main/java/swyp/team5/greening/comment/domain/entity/Comment.java
@@ -0,0 +1,29 @@
+package swyp.team5.greening.comment.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import swyp.team5.greening.common.base.BaseTimeEntity;
+
+@Entity
+@Table(name = "comments")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+}
