cf5ceddbc44cc009ff3b6fd9fa8306e33b765b70
kkang_h00n <kig2454@gmail.com>
2025-04-28 20:57:01 +0900

    feat: JPA Auditing 셋팅

---
+++ src/main/java/swyp/team5/greening/common/base/AuditingConfig.java
@@ -0,0 +1,10 @@
+package swyp.team5.greening.common.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+}
