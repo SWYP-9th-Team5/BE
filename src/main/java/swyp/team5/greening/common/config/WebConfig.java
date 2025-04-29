b0bac5e400b572431894d91280be028fd26bf0ac
kkang_h00n <kig2454@gmail.com>
2025-04-29 20:31:04 +0900

    Feat: Cors 설정 추가

--- src/main/java/swyp/team5/greening/common/config/WebConfig.java
+++ src/main/java/swyp/team5/greening/common/config/WebConfig.java
@@ -1,9 +1,13 @@
 package swyp.team5.greening.common.config;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import swyp.team5.greening.common.resolver.LogInUserArgumentResolver;
 
@@ -16,6 +20,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(logInUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(AUTHORIZATION, CONTENT_TYPE)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
 }
