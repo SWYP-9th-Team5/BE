1adb92946d8c9cb8b3e425790003f376f2fca89f
kkang_h00n <kig2454@gmail.com>
2025-05-01 16:31:19 +0900

    Edit: 유저 정보가 필요한 API에서 JWT 토큰이 안남어올 경우 예외 처리 로직 추가

--- src/main/java/swyp/team5/greening/auth/exception/AuthExceptionMessage.java
+++ src/main/java/swyp/team5/greening/auth/exception/AuthExceptionMessage.java
@@ -12,7 +12,8 @@ public enum AuthExceptionMessage implements ExceptionMessage {
     AUTH_TOKEN_EXPIRED("기한이 만료된 토큰입니다", "401"),
     AUTH_TOKEN_UNSUPPORTED("지원하지 않는 토큰입니다", "401"),
     AUTH_TOKEN_ILLEGAL("claims 정보가 비어있습니다", "401"),
-    AUTH_TOKEN_NOT_SIGNATURE("Jwt 서명이 로컬로 산정된 서명과 일치하지 않습니다", "401");
+    AUTH_TOKEN_NOT_SIGNATURE("Jwt 서명이 로컬로 산정된 서명과 일치하지 않습니다", "401"),
+    UNAUTHORIZED("로그인이 필요한 서비스입니다.", "401");
 
     private final String message;
     private final String code;
--- src/main/java/swyp/team5/greening/common/resolver/LogInUserArgumentResolver.java
+++ src/main/java/swyp/team5/greening/common/resolver/LogInUserArgumentResolver.java
@@ -1,6 +1,7 @@
 package swyp.team5.greening.common.resolver;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import swyp.team5.greening.auth.exception.AuthException;
+import swyp.team5.greening.auth.exception.AuthExceptionMessage;
 import swyp.team5.greening.common.exception.GreeningGlobalException;
 import swyp.team5.greening.user.domain.repository.UserRepository;
 import swyp.team5.greening.user.exception.UserExceptionMessage;
@@ -32,6 +35,11 @@ public class LogInUserArgumentResolver implements HandlerMethodArgumentResolver 
 
         //필터에서 parsing 한 userId 조회
         Long userId = (Long) request.getAttribute("authorize-user-id");
+
+        //만약 user 정보가 요청 정보에 담겨있지 않다면, 인증 예외
+        if (Objects.isNull(userId)) {
+            throw new AuthException(AuthExceptionMessage.UNAUTHORIZED);
+        }
 
         //존재하지 않는 유저라면
         if (!userRepository.existsById(userId)) {
