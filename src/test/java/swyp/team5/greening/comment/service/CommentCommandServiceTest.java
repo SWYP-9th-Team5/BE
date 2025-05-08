62e16ab80f8ed4db3e153642cce1e018323f714d
kkang_h00n <kig2454@gmail.com>
2025-05-09 00:13:04 +0900

    Test: 댓글 수정 기능 단위 테스트

--- src/test/java/swyp/team5/greening/comment/service/CommentCommandServiceTest.java
+++ src/test/java/swyp/team5/greening/comment/service/CommentCommandServiceTest.java
@@ -3,6 +3,7 @@ package swyp.team5.greening.comment.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Optional;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -15,8 +16,10 @@ import org.springframework.test.util.ReflectionTestUtils;
 import swyp.team5.greening.comment.domain.entity.Comment;
 import swyp.team5.greening.comment.domain.repository.CommentRepository;
 import swyp.team5.greening.comment.dto.request.SaveCommentRequestDto;
+import swyp.team5.greening.comment.dto.request.UpdateCommentRequestDto;
 import swyp.team5.greening.comment.dto.response.SaveCommentResponseDto;
-import swyp.team5.greening.comment.exception.PostExceptionMessage;
+import swyp.team5.greening.comment.exception.CommentExceptionMessage;
+import swyp.team5.greening.post.exception.PostExceptionMessage;
 import swyp.team5.greening.common.exception.GreeningGlobalException;
 import swyp.team5.greening.post.domain.repository.PostRepository;
 
@@ -79,6 +82,54 @@ class CommentCommandServiceTest {
             assertThatThrownBy(when)
                     .isInstanceOf(GreeningGlobalException.class)
                     .hasMessage(PostExceptionMessage.NOT_FOUND_POST.getMessage());
+        }
+    }
+
+    @DisplayName("댓글 수정 및 삭제 테스트")
+    @Nested
+    class updateAndDeleteCommentTest {
+
+        Long postId = 1L;
+        Long userId = 100L;
+        Long commentId = 2L;
+        String comment = "테스트 댓글입니다";
+        String newComment = "수정한 댓글입니다";
+
+        Comment testComment = Comment.builder()
+                .postId(postId)
+                .userId(userId)
+                .comment(comment)
+                .build();
+
+        @Test
+        @DisplayName("댓글 내용이 알맞게 변경된다.")
+        void testCase1() {
+            //given
+            given(commentRepository.findById(commentId)).willReturn(Optional.of(testComment));
+
+            //when
+            commentCommandService.updateComment(userId, new UpdateCommentRequestDto(commentId, newComment));
+
+            //then
+            assertThat(testComment.getComment()).isEqualTo(newComment);
+            verify(commentRepository, times(1)).findById(commentId);
+        }
+
+        @Test
+        @DisplayName("작성자가 아닌 사용자가 변경 시도 시, 예외가 발생한다.")
+        void testCase2() {
+            //given
+            Long anotherUserId = 30L;
+            given(commentRepository.findById(commentId)).willReturn(Optional.of(testComment));
+
+            //when
+            ThrowingCallable throwingCallable = () -> commentCommandService.updateComment(anotherUserId,
+                    new UpdateCommentRequestDto(commentId, newComment));
+
+            //then
+            assertThatThrownBy(throwingCallable)
+                    .isInstanceOf(GreeningGlobalException.class)
+                    .hasMessageContaining(CommentExceptionMessage.BAD_REQUEST_COMMENT_WRITER.getMessage());
         }
     }
 }
