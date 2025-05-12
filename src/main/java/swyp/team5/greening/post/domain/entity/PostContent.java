2dd0487991903331902ce8434458c718dd44ce62
kkang_h00n <kig2454@gmail.com>
2025-05-12 15:11:28 +0900

    Edit: 게시글 내용 엔티티 설정
    - 게시글 이미지 엔티티 -> 게시글 내용 엔티티 변경
    - 내용에 대한 순서 및 이미지 & 텍스트 세분화

--- src/main/java/swyp/team5/greening/post/domain/entity/PostContent.java
+++ src/main/java/swyp/team5/greening/post/domain/entity/PostContent.java
@@ -2,6 +2,8 @@ package swyp.team5.greening.post.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,16 +14,23 @@ import jakarta.persistence.Table;
 import swyp.team5.greening.common.base.BaseTimeEntity;
 
 @Entity
-@Table(name = "post_images")
-public class PostImage extends BaseTimeEntity {
+@Table(name = "post_contents")
+public class PostContent extends BaseTimeEntity {
 
     @Id
     @Column(name = "post_image_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "post_image_url")
-    private String postImageUrl;
+    @Column(name = "content")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private PostType type;
+
+    @Column(name = "sequence")
+    private Integer sequence;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
