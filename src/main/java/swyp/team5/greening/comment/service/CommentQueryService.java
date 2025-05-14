30097bca90d47208707508ca55f0af9e17b144e2
kkang_h00n <kig2454@gmail.com>
2025-05-14 19:15:30 +0900

    Fix: 댓글 조회 시, 읽기 전용 트랜잭션 적용

--- src/main/java/swyp/team5/greening/comment/service/CommentQueryService.java
+++ src/main/java/swyp/team5/greening/comment/service/CommentQueryService.java
@@ -1,10 +1,10 @@
 package swyp.team5.greening.comment.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import swyp.team5.greening.comment.domain.repository.CommentQueryRepository;
 import swyp.team5.greening.comment.dto.response.FindAllCommentResponseDto;
 import swyp.team5.greening.post.exception.PostExceptionMessage;
@@ -18,7 +18,7 @@ public class CommentQueryService {
     private final CommentQueryRepository commentQueryRepository;
     private final PostRepository postRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<FindAllCommentResponseDto> findAllComment(
             Long userId, Long postId,
             Integer pageNumber, Integer pageSize
