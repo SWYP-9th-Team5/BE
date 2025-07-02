3cab4e1d43f3e240e80bf56d9f1188f791113b77
kkang_h00n <kig2454@gmail.com>
2025-07-02 21:44:42 +0900

    Feat: 특정 애완 식물 삭제 API 구현

--- src/main/java/swyp/team5/greening/petPlant/domain/repository/PetPlantRepository.java
+++ src/main/java/swyp/team5/greening/petPlant/domain/repository/PetPlantRepository.java
@@ -1,9 +1,13 @@
 package swyp.team5.greening.petPlant.domain.repository;
 
+import java.util.Optional;
 import swyp.team5.greening.petPlant.domain.entity.PetPlant;
+import swyp.team5.greening.petPlant.domain.entity.PetPlantState;
 
 public interface PetPlantRepository {
 
     PetPlant save(PetPlant petPlant);
+
+    Optional<PetPlant> findByIdAndState(Long id, PetPlantState state);
 
 }
--- src/main/java/swyp/team5/greening/petPlant/service/PetPlantCommandService.java
+++ src/main/java/swyp/team5/greening/petPlant/service/PetPlantCommandService.java
@@ -1,13 +1,16 @@
 package swyp.team5.greening.petPlant.service;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import swyp.team5.greening.common.exception.GreeningGlobalException;
 import swyp.team5.greening.petPlant.domain.entity.PetPlant;
 import swyp.team5.greening.petPlant.domain.entity.PetPlantState;
 import swyp.team5.greening.petPlant.domain.repository.PetPlantRepository;
 import swyp.team5.greening.petPlant.dto.request.CreatePetPlantRequestDto;
 import swyp.team5.greening.petPlant.dto.response.CreatePetPlantResponseDto;
+import swyp.team5.greening.petPlant.exception.PetPlantExceptionMessage;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +34,25 @@ public class PetPlantCommandService {
         petPlantRepository.save(petPlant);
 
         return new CreatePetPlantResponseDto(petPlant.getId());
+    }
+
+    //애완 식물 삭제
+    @Transactional
+    public void deletePetPlant(
+            Long userId,
+            Long petPlantId
+    ) {
+        PetPlant petPlant = petPlantRepository.findByIdAndState(petPlantId,
+                        PetPlantState.IN_PROGRESS)
+                .orElseThrow(() -> new GreeningGlobalException(
+                        PetPlantExceptionMessage.NOT_FOUND_PET_PLANT));
+
+        if (!Objects.equals(petPlant.getUserId(), userId)) {
+            throw new GreeningGlobalException(
+                    PetPlantExceptionMessage.BAD_REQUEST_PET_PLANT_WRITER);
+        }
+
+        petPlant.deletePetPlant();
     }
 
 }
