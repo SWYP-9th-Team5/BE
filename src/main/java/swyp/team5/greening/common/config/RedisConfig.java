dff7c6f3e3877d352dece5dda1c9cc5400f57b2f
kkang_h00n <kig2454@gmail.com>
2025-07-12 21:53:21 +0900

    Feat: 홈 게시물 조회 시 캐시 적용

--- src/main/java/swyp/team5/greening/common/config/RedisConfig.java
+++ src/main/java/swyp/team5/greening/common/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package swyp.team5.greening.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
@@ -11,7 +14,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -33,6 +36,10 @@ public class RedisConfig {
     //캐시 설정
     @Bean
     public CacheManager postCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
                 .defaultCacheConfig()
                 // Redis에 Key를 저장할 때 String으로 직렬화(변환)해서 저장
@@ -42,11 +49,11 @@ public class RedisConfig {
                 // Redis에 Value를 저장할 때 Json으로 직렬화(변환)해서 저장
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair.fromSerializer(
-                                new Jackson2JsonRedisSerializer<Object>(Object.class)
+                                new GenericJackson2JsonRedisSerializer(objectMapper)
                         )
                 )
                 // 데이터의 만료기간(TTL) 설정
-                .entryTtl(Duration.ofMinutes(1L));
+                .entryTtl(Duration.ofMinutes(15L));
 
         return RedisCacheManager
                 .RedisCacheManagerBuilder
